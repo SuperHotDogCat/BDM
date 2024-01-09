b82c281915e0b2d9562129a002fa721412568d23
SuperHotDogCat <siromisochan@gmail.com>
2024-01-09 17:03:45 +0900

    htmlとjs書き終わり

---
+++ main.js
@@ -0,0 +1,19 @@
+let url = "./logs.json"
+let command = "";
+let latest_time; //undefined
+await fetch(url)
+   .then((response) => {
+       return response.json()　//ここでBodyからJSONを返す
+   })
+   .then((result) => {
+    const latestCommand = result.reduce((latest, current) => {
+        const latestTime = new Date(latest.time);
+        const currentTime = new Date(current.time);
+      
+        return latestTime > currentTime ? latest : current;
+      }, result[0]); 
+    command = latestCommand.command;
+    console.log(command)
+   })
+let elem = document.getElementById("text");
+elem.textContent = `飲酒🍺${command}は`;
